a70b8e84547ec6d1eabefb2cab8d4e9914cc573f
Opensourceforlife <opensourceforlife@gmail.com>
2013-03-28 17:41:09 +0530

    Modified for userPrivatePage issue
    When we set custom.landing.page.type=userPrivatePage and if user is
    logged in first time, it's not able to take user to its private page. As
    on first page load when it actually redirects/generate the privatePage,
    page doesn't exist. Once user passed through terms & condition and all
    other process after first time login, it will generate user's public &
    private page. So this code change will allow first time logged user to
    take them on private/public page. Thanks to vic for pointing this issue.

--- maven/6.1.1CEGA2/CustomLandingPage-hook/src/main/java/com/liferay/opensourceforlife/landingpage/UserPrivatePageType.java
+++ maven/6.1.1CEGA2/CustomLandingPage-hook/src/main/java/com/liferay/opensourceforlife/landingpage/UserPrivatePageType.java
@@ -9,31 +9,21 @@ import javax.servlet.http.HttpServletRequest;
 import com.liferay.opensourceforlife.util.CustomLandingPageUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
-import com.liferay.portal.kernel.util.StringPool;
-import com.liferay.portal.model.User;
-import com.liferay.portal.util.PortalUtil;
 
 /**
  * @author tejas.kanani
  */
-public class UserPrivatePageType extends LandingPageType {
+public class UserPrivatePageType extends LandingPageType
+{
 
 	/*
 	 * (non-Javadoc)
 	 * @see com.liferay.opensourceforlife.landingpage.LandingPageType#getLandingPagePath (javax.portlet.PortletRequest)
 	 */
 	@Override
-	public String getLandingPagePath(final HttpServletRequest request)
-			throws PortalException, SystemException {
-
-		String userPrivatePagePath = StringPool.BLANK;
-
-		User currentUser = PortalUtil.getUser(request);
-
-		if (currentUser.hasPrivateLayouts()) {
-			userPrivatePagePath = CustomLandingPageUtil.getDisplayURL(request,
-					true);
-		}
-		return userPrivatePagePath;
+	public String getLandingPagePath(final HttpServletRequest request) throws PortalException,
+			SystemException
+	{
+		return CustomLandingPageUtil.getDisplayURL(request, true);
 	}
 }
--- maven/6.1.1CEGA2/CustomLandingPage-hook/src/main/java/com/liferay/opensourceforlife/util/CustomLandingPageUtil.java
+++ maven/6.1.1CEGA2/CustomLandingPage-hook/src/main/java/com/liferay/opensourceforlife/util/CustomLandingPageUtil.java
@@ -27,22 +27,31 @@ import com.liferay.portal.util.PortalUtil;
 /**
  * @author tejas.kanani
  */
-public final class CustomLandingPageUtil {
+public final class CustomLandingPageUtil
+{
 
-	private CustomLandingPageUtil() {
+	private CustomLandingPageUtil()
+	{
 	}
 
-	public static String getLanguage(final HttpServletRequest request)
-			throws PortalException, SystemException {
+	/**
+	 * @param request
+	 * @return
+	 * @throws PortalException
+	 * @throws SystemException
+	 */
+	public static String getLanguage(final HttpServletRequest request) throws PortalException,
+			SystemException
+	{
 
 		String language = StringPool.BLANK;
 		Locale currentLocale = PortalUtil.getUser(request).getLocale();
 
-		if (GetterUtil.getInteger(PropsUtil
-				.get(PropsKeys.LOCALE_PREPEND_FRIENDLY_URL_STYLE)) == 1
+		if (GetterUtil.getInteger(PropsUtil.get(PropsKeys.LOCALE_PREPEND_FRIENDLY_URL_STYLE)) == 1
 				&& !currentLocale.equals(LocaleUtil.getDefault())
-				|| GetterUtil.getInteger(PropsUtil
-						.get(PropsKeys.LOCALE_PREPEND_FRIENDLY_URL_STYLE)) == 2) {
+				|| GetterUtil
+						.getInteger(PropsUtil.get(PropsKeys.LOCALE_PREPEND_FRIENDLY_URL_STYLE)) == 2)
+		{
 
 			language = buildI18NPath(currentLocale);
 		}
@@ -50,68 +59,133 @@ public final class CustomLandingPageUtil {
 		return language;
 	}
 
-	private static String buildI18NPath(final Locale locale) {
+	/**
+	 * @param locale
+	 * @return
+	 */
+	private static String buildI18NPath(final Locale locale)
+	{
 
 		String languageId = LocaleUtil.toLanguageId(locale);
 
-		if (Validator.isNull(languageId)) {
+		if (Validator.isNull(languageId))
+		{
 			return null;
 		}
 
-		if (LanguageUtil.isDuplicateLanguageCode(locale.getLanguage())) {
-			Locale priorityLocale = LanguageUtil
-					.getLocale(locale.getLanguage());
+		if (LanguageUtil.isDuplicateLanguageCode(locale.getLanguage()))
+		{
+			Locale priorityLocale = LanguageUtil.getLocale(locale.getLanguage());
 
-			if (locale.equals(priorityLocale)) {
+			if (locale.equals(priorityLocale))
+			{
 				languageId = locale.getLanguage();
 			}
-		} else {
+		} else
+		{
 			languageId = locale.getLanguage();
 		}
 
 		return StringPool.SLASH.concat(languageId);
 	}
 
+	/**
+	 * Generate displayURL for specified group
+	 * 
+	 * @param request
+	 * @param isPrivateLayout
+	 * @return
+	 * @throws PortalException
+	 * @throws SystemException
+	 */
 	public static String getDisplayURL(final HttpServletRequest request,
-			final boolean isPrivateLayout) throws PortalException,
-			SystemException {
+			final boolean isPrivateLayout) throws PortalException, SystemException
+	{
 
 		String displayURL = StringPool.BLANK;
 
-		Group group = GroupLocalServiceUtil
-				.getUserGroup(PortalUtil.getCompanyId(request),
-						PortalUtil.getUserId(request));
+		Group group = GroupLocalServiceUtil.getUserGroup(PortalUtil.getCompanyId(request),
+				PortalUtil.getUserId(request));
 
-		int publicLayoutsPageCount = group.getPublicLayoutsPageCount();
+		boolean isLayoutCountEmpty = true;
 
-		if (publicLayoutsPageCount > 0) {
-			StringBundler sb = new StringBundler(CustomLandingPageConstant.FIVE);
+		if (isPrivateLayout && group.getPrivateLayoutsPageCount() > 0)
+		{
+			isLayoutCountEmpty = false;
+		} else if (!isPrivateLayout && group.getPublicLayoutsPageCount() > 0)
+		{
+			isLayoutCountEmpty = false;
+		}
 
-			sb.append(PortalUtil.getPathMain());
-			sb.append("/my_sites/view?groupId=");
-			sb.append(group.getGroupId());
-
-			if (isPrivateLayout) {
-				sb.append("&privateLayout=1");
-			} else {
-				sb.append("&privateLayout=0");
-			}
-
-			displayURL = sb.toString();
+		if (!isLayoutCountEmpty)
+		{
+			displayURL = generateLayoutDisplayUrl(group, isPrivateLayout);
+		} else
+		{
+			displayURL = generateUserLayoutDisplayUrl(group, isPrivateLayout);
 		}
 
 		return displayURL;
 	}
 
-	public static List<Group> getSites(final long userId)
-			throws PortalException, SystemException {
+	/**
+	 * Generate layout DisplayURL for User's public/private page
+	 * 
+	 * @param group
+	 * @param isPrivateLayout
+	 * @return
+	 */
+	private static String generateLayoutDisplayUrl(final Group group, final boolean isPrivateLayout)
+	{
+		StringBundler sb = new StringBundler(CustomLandingPageConstant.FIVE);
+
+		sb.append(PortalUtil.getPathMain());
+		sb.append("/my_sites/view?groupId=");
+		sb.append(group.getGroupId());
+		if (isPrivateLayout)
+		{
+			sb.append("&privateLayout=1");
+		} else
+		{
+			sb.append("&privateLayout=0");
+		}
+
+		return sb.toString();
+	}
+
+	/**
+	 * @param group
+	 * @param isPrivateLayout
+	 * @return
+	 */
+	private static String generateUserLayoutDisplayUrl(final Group group,
+			final boolean isPrivateLayout)
+	{
+		StringBundler sb = new StringBundler(CustomLandingPageConstant.FIVE);
+		if (isPrivateLayout)
+		{
+			sb.append(PortalUtil.getPathFriendlyURLPrivateUser());
+		} else
+		{
+			sb.append(PortalUtil.getPathFriendlyURLPublic());
+		}
+
+		sb.append(group.getFriendlyURL());
+
+		return sb.toString();
+	}
+
+	public static List<Group> getSites(final long userId) throws PortalException, SystemException
+	{
 
 		List<Group> sites = new ArrayList<Group>();
 
-		for (Group group : GroupLocalServiceUtil.getUserGroups(userId)) {
+		for (Group group : GroupLocalServiceUtil.getUserGroups(userId))
+		{
 			if (group.isRegularSite()
-					&& !CustomLandingPageConstant.GUEST_GROUP_FRIENDLY_URL
-							.equalsIgnoreCase(group.getFriendlyURL())) {
+					&& !CustomLandingPageConstant.GUEST_GROUP_FRIENDLY_URL.equalsIgnoreCase(group
+							.getFriendlyURL()))
+			{
 				sites.add(group);
 				break;
 			}
